63f9aaf1c1a88e4e54cd03e65e0990525eee8892
MANISH LANJEWAR <manishlanjewar8@gmail.com>
2022-09-27 09:55:14 +0530

    added factors and quotient program

---
+++ src/com/bridgelabz/Quotient_Rmainder.java
@@ -0,0 +1,20 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class Quotient_Rmainder {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Number1 : ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter Number2 : ");
+        int num2 = scanner.nextInt();
+
+        int quotient = num1 / num2;
+        int remainder = num1 % num2;
+
+        System.out.println("Quotient = " + quotient);
+        System.out.println("Remainder = " + remainder);
+    }
+}
